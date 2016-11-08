63ad92396aed5e8b019e3da8a3e4067985bc3787
jkrouse13 <jon.krouse@gmail.com>
2016-11-07 20:56:39 -0500

    Added bad movies

--- main.js
+++ main.js
@@ -5,7 +5,8 @@ movies.push(
   genre: "Action-Thriller",
   year: "2006",
   actor_list: ["Daniel Craig", "Judi Dench", "Eva Green", "Mads Mikkelsen"],
-  stars: "8.0"}
+  stars: "8.0",
+  metascore: "80"}
 )
 movies.push(
   {title: "The Muppet Christmas Carol",
@@ -14,7 +15,7 @@ movies.push(
   year: "1992",
   actor_list: ["Michael Caine", "Frank Oz"],
   stars: "7.7",
-  metascore: "80"}
+  metascore: "n/a"}
 )
 movies.push(
   {title: "Ocean's Thirteen",
@@ -43,6 +44,53 @@ movies.push(
   stars: "7.5",
   metascore: "63"}
 )
+movies.push(
+  {title: "Muppet Treasure Island ",
+  director_list: "Brian Henson",
+  genre: "Comedy",
+  year: "1996",
+  actor_list: ["Tim Curry", "Billy Connolly", "Frank Oz"],
+  stars: "6.9",
+  metascore: "n/a"}
+)
+movies.push(
+  {title: "Pirates of the Caribbean: On Stranger Tides",
+  director_list: "Rob Marshall",
+  genre: "Action-Adventure",
+  year: "2011",
+  actor_list: ["Johnny Depp", "Penélope Cruz", "Ian McShane"],
+  stars: "6.7",
+  metascore: "45"}
+)
+movies.push(
+  {title: "Cars 2",
+  director_list: "John Lassiter",
+  genre: "Animated",
+  year: "2011",
+  actor_list: ["Owen Wilson", "Larry the Cable Guy", "Michael Caine"],
+  stars: "6.3",
+  metascore: "57"}
+)
+movies.push(
+  {title: "Indiana Jones and the Kingdom of the Crystal Skull",
+  director_list: "Steven Spielberg",
+  genre: "Action-Adventure",
+  year: "2008",
+  actor_list: ["Harrison Ford", "Cate Blanchett", "Shia LaBeouf"],
+  stars: "6.2",
+  metascore: "65"}
+)
+movies.push(
+  {title: "Dumb and Dumber To",
+  director_list: "Bobby Farrelly",
+  genre: "Comedy",
+  year: "2014",
+  actor_list: ["Jim Carrey", "Jeff Daniels", "Rob Riggle"],
+  stars: "5.7",
+  metascore: "36"}
+)
+
+
 
 function movie_sort(movie, movie2){
     if(movie.title < movie2.title) return -1
